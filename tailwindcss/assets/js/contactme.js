d847f40152e9ae7cc061e7eb5c79fd012727dfdc
Kel Ming <kkgm013@gmail.com>
2024-10-24 15:31:31 +0100

    Fixed Flash Animation Issue with Contact Us Click + Started About Page

--- tailwindcss/assets/js/contactme.js
+++ tailwindcss/assets/js/contactme.js
@@ -1,19 +1,24 @@
-document.getElementById('contactLink').addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.getElementById('contactme').scrollIntoView({
-        behavior: 'smooth'
-    });
-
-    const svgIcons = document.querySelectorAll('#contactme svg');
-
-    svgIcons.forEach(icon => {
-        icon.classList.add('animate-flash');
-    });
-
-    setTimeout(() => {
-        svgIcons.forEach(icon => {
-            icon.classList.remove('animate-flash');
+document.addEventListener('DOMContentLoaded', () => {
+    const a = document.getElementById('contactLink');
+    const b = document.querySelectorAll('#contactme svg');
+    if(a) {
+        a.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.getElementById('contactme').scrollIntoView({
+                behavior: 'smooth'
+            });
+        
+            b.forEach(c => {
+                c.classList.remove('animate-none');
+                c.classList.add('animate-flash');
+            });
+        
+            setTimeout(() => {
+                b.forEach(icon => {
+                    c.classList.remove('animate-flash');
+                    c.classList.add('animate-none');
+                });
+            }, 3000); 
         });
-    }, 3000); 
+    }
 });
