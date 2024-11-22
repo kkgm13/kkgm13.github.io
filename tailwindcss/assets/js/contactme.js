5cefbb2ddd84c0a14ed663a91f8b6f1ebec08efa
Kel Ming <kkgm013@gmail.com>
2024-11-22 11:37:06 +0000

    SVG icons of tools in Skills where relevant + updated contact me information for about.html (rest of the pages will need to be created. Vue Consideration?)

--- tailwindcss/assets/js/contactme.js
+++ tailwindcss/assets/js/contactme.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const a = document.getElementById('contactLink');
+    const a2 = document.getElementById('contactLinkMobile');
     const b = document.querySelectorAll('#contactme svg');
     if(a) {
         a.addEventListener('click', function (e) {
@@ -21,26 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 3000); 
         });
     }
+    if(a2){
+        a2.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.getElementById('contactme').scrollIntoView({
+                behavior: 'smooth'
+            });
+        
+            b.forEach(c => {
+                c.classList.remove('animate-none');
+                c.classList.add('animate-flash');
+            });
+        
+            setTimeout(() => {
+                b.forEach(c => {
+                    c.classList.remove('animate-flash');
+                    c.classList.add('animate-none');
+                });
+            }, 3000); 
+        });
+    }
 });
-
-// document.getElementById('contactLink').addEventListener('click', function (e) {
-//   e.preventDefault();
-
-//   document.getElementById('contactme').scrollIntoView({
-//       behavior: 'smooth'
-//   });
-
-//   const svgIcons = document.querySelectorAll('#contactme svg');
-
-//   svgIcons.forEach(icon => {
-//       icon.classList.remove('animate-none');
-//       icon.classList.add('animate-flash');
-//   });
-
-//   setTimeout(() => {
-//       svgIcons.forEach(icon => {
-//           icon.classList.remove('animate-flash');
-//           icon.classList.add('animate-none');
-//       });
-//   }, 3000); 
-// });
